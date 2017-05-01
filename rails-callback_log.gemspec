ed327da92c8f8d43943afb1da04a8dc94e33ce16
Jared Beck <jared@jaredbeck.com>
2017-05-01 17:57:56 -0400

    Add support for rails 5.1

--- rails-callback_log.gemspec
+++ rails-callback_log.gemspec
@@ -11,12 +11,17 @@ require "rails_callback_log/version"
   spec.summary = "Logs callbacks to help with debugging."
   spec.homepage = "https://github.com/jaredbeck/rails-callback_log"
   spec.license = "MIT"
-  spec.files = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.bindir = "exe"
-  spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.files = [
+    "Gemfile", # necessary?
+    "LICENSE.txt", # because: lawyers
+    "lib/rails-callback_log.rb",
+    "lib/rails_callback_log/version.rb",
+    "rails-callback_log.gemspec"
+  ]
   spec.require_paths = ["lib"]
   spec.required_ruby_version = ">= 2.0"
-  spec.add_runtime_dependency "activesupport", [">= 4.2.0", "< 5.1"]
+  spec.add_runtime_dependency "activesupport", [">= 4.2.0", "< 5.2"]
+  spec.add_development_dependency "appraisal", "~> 2.2"
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
--- spec/rails-callback_log_spec.rb
+++ spec/rails-callback_log_spec.rb
@@ -2,37 +2,69 @@ require "spec_helper"
 
 module ActiveSupport
   module Callbacks
-    RSpec.describe Callback do
-      describe "#make_lambda_with_log" do
-        it "returns a Proc that calls the rails logger" do
-          # Instantiate a `Callback`. For this test, none of the arguments matter.
-          name = double.as_null_object
-          filter = double.as_null_object
-          kind = double.as_null_object
-          options = double.as_null_object
-          chain_config = double.as_null_object
-          cb = described_class.new(name, filter, kind, options, chain_config)
+    if ::ActiveSupport.gem_version >= ::Gem::Version.new("5.1.0")
+      RSpec.describe CallTemplate do
+        describe "#make_lambda" do
+          it "returns a Proc that calls the rails logger" do
+            callback = double("mock callback")
+            cb = described_class.build(:banana, callback)
 
-          # Expect `make_lambda` to return a `Proc`.
-          lambda_with_log = cb.make_lambda(:banana)
-          expect(lambda_with_log).to be_a(::Proc)
+            # Expect `make_lambda` to return a `Proc`.
+            lambda_with_log = cb.make_lambda
+            expect(lambda_with_log).to be_a(::Proc)
 
-          # Mock the rails logger. We're going to expect it to receive a `debug` message.
-          logger = double
-          allow(logger).to receive(:debug)
-          allow(::Rails).to receive(:logger).and_return(logger)
-          target = double
+            # Mock the rails logger. We're going to expect it to receive a `debug` message.
+            logger = double
+            allow(logger).to receive(:debug)
+            allow(::Rails).to receive(:logger).and_return(logger)
+            target = double
 
-          # Symbol-based callbacks have a `target` and a `value`. The definition of
-          # these arguments is not clear to me yet. My guess is an AR callback's `target`
-          # would be a model instance, for example.
-          allow(target).to receive(:kiwi).with(:banana)
-          allow(target).to receive(:banana)
+            # Symbol-based callbacks have a `target` and a `value`. The definition of
+            # these arguments is not clear to me yet. My guess is an AR callback's `target`
+            # would be a model instance, for example.
+            allow(target).to receive(:kiwi).with(:banana)
+            allow(target).to receive(:banana)
 
-          # Call our lambda and expect it to send a debug message to our mock logger.
-          lambda_with_log.call(target, :kiwi)
-          expect(logger).to have_received(:debug).once.with("Callback: banana")
-          expect(target).to have_received(:banana).once
+            # Call our lambda and expect it to send a debug message to our mock logger.
+            lambda_with_log.call(target, :kiwi)
+            expect(logger).to have_received(:debug).once.with("Callback: banana")
+            expect(target).to have_received(:banana).once
+          end
+        end
+      end
+    else
+      RSpec.describe Callback do
+        describe "#make_lambda" do
+          it "returns a Proc that calls the rails logger" do
+            # Instantiate a `Callback`. For this test, none of the arguments matter.
+            name = double.as_null_object
+            filter = double.as_null_object
+            kind = double.as_null_object
+            options = double.as_null_object
+            chain_config = double.as_null_object
+            cb = described_class.new(name, filter, kind, options, chain_config)
+
+            # Expect `make_lambda` to return a `Proc`.
+            lambda_with_log = cb.make_lambda(:banana)
+            expect(lambda_with_log).to be_a(::Proc)
+
+            # Mock the rails logger. We're going to expect it to receive a `debug` message.
+            logger = double
+            allow(logger).to receive(:debug)
+            allow(::Rails).to receive(:logger).and_return(logger)
+            target = double
+
+            # Symbol-based callbacks have a `target` and a `value`. The definition of
+            # these arguments is not clear to me yet. My guess is an AR callback's `target`
+            # would be a model instance, for example.
+            allow(target).to receive(:kiwi).with(:banana)
+            allow(target).to receive(:banana)
+
+            # Call our lambda and expect it to send a debug message to our mock logger.
+            lambda_with_log.call(target, :kiwi)
+            expect(logger).to have_received(:debug).once.with("Callback: banana")
+            expect(target).to have_received(:banana).once
+          end
         end
       end
     end
